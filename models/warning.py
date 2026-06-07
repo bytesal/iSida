884ec897291fc45409947192d37354fe560908d9
Sal <35301371+bytesal@users.noreply.github.com>
2026-06-07 16:06:26 +0300

    Create warning.py

---
+++ models/warning.py
@@ -0,0 +1,11 @@
+"""
+Warning Document Schema (MongoDB)
+{
+    "_id": ObjectId,
+    "guild_id": int,
+    "user_id": int,
+    "moderator_id": int,
+    "reason": str,
+    "timestamp": datetime
+}
+"""
